713b019a535af45398e01a3a88c91d185685c5ce
N-fire <55892717+N-fire@users.noreply.github.com>
2022-03-16 22:33:57 +0100

    feat: initial comit

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name "nfire_hunting"
+description "Hunting Script made for ox_inventory"
+author "Nfire"
+version "1.0.0"
+
+shared_scripts {
+	'config.lua',
+	'locales/*.lua'
+}
+
+client_scripts {
+	'client/*.lua'
+}
+
+server_scripts {
+	'server/*.lua'
+}
